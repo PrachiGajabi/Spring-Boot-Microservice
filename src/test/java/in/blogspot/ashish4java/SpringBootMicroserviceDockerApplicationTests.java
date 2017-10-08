dbcd3bdcb3e7d451740f222f2e81612768424762
Prachi Gajabi <31819043+PrachiGajabi@users.noreply.github.com>
2017-10-08 21:50:23 +1100

    Add files via upload

---
+++ src/test/java/in/blogspot/ashish4java/SpringBootMicroserviceDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package in.blogspot.ashish4java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMicroserviceDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
